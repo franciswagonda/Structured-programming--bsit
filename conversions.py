a5ad4e828cf3f7410b212d9e7a8b32c5ad5aaa13
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-08 23:17:56 +0300

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,15 @@
+number_two = 3.17 #float
+
+new_number_two = int(number_two)
+print(f"integer: {new_number_two}")
+
+
+
+
+age = 8 ##  convert to a float
+
+
+age = 8
+new_age = float(age)
+print(f"float: {new_age}")
+
