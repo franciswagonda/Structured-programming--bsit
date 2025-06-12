8d4e31c04f814ddf6c7ccf891f3610db7f09490d
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-12 06:36:58 +0300

    Add files via upload

---
+++ number_one.py
@@ -0,0 +1,45 @@
+#Question One (6 Marks)
+#Differentiate Between Assigning and Declaring a variable giving practical Examples of Each 
+"""
+Assigning a variable means putting a specific value into that reserved space
+using workks with an operator
+
+for example
+age = 8
+
+
+while
+
+Declaring a variable means reserving a space in memory to store a value and giving that space a name
+
+for example
+
+a = 10        # Declaring an integer variable named 'a'
+
+b = 20.5      # Declaring a float variable named 'b' 
+
+c = 'hello'   # Declaring a string variable named 'c'
+
+"""
+
+##Question Two (4 Marks)
+##Given the code below
+
+##age = "8"
+##length = “56.90”
+##height = “34.09944”
+## convert the variable age to integer datatype
+
+age = 8
+new_age = float(age)
+print(f"float: {new_age}")
+
+
+length = 56.90
+new_length = int(length)
+print(f'integer: {new_length}')
+
+
+height = 34.0994
+new_height = int(height)
+print(f"integer: {new_height}")
