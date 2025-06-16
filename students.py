83b05b152da167f232de6dc2d0180018cda72b08
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-16 22:34:47 +0300

    Add files via upload

---
+++ students.py
@@ -0,0 +1,15 @@
+# print ("enter number students")
+# students = int(input("students:"))
+# if students < 0:
+#     print("invalid, enter valid number of sttuents")
+# elif students < 100:
+#     print ("repeat")
+# elif students >= 150:
+#     print ("granduate")
+# elif students >= 100 and students <= 150:
+#     print ("retain")
+
+
+
+age = 8
+print("age")
