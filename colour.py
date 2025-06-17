211517ec1742313db339baeef1119b8b322966cf
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-17 21:11:33 +0300

    Add files via upload

---
+++ colour.py
@@ -0,0 +1,27 @@
+
+print("enter faourite color")
+color = input("color:")
+if color == "red":
+    print("red is a your favourite color")
+elif color == "blue":
+    print ("Blue is your favourite color")
+elif color == "black":
+    print ("Black is your favourite color")
+elif color == "green":
+    print ("green is your favourite color")
+elif color == "yellow":
+    print ("yellow is your favourite color")    
+elif color == "purple":
+    print ("purple is your favourite color")
+elif color == "orange":
+    print ("orange is your favourite color")
+elif color == "pink":   
+    print ("pink is your favourite color")  
+elif color == "brown":
+    print ("brown is your favourite color")
+elif color == "white":
+    print ("white is your favourite color") 
+elif color == "gray":
+    print ("gray is your favourite color")
+else:
+    print("favourite not found")
