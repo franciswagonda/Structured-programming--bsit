3a172b3c106cafe901e5240fb675f25ebac7b2c6
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-19 21:46:19 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,20 @@
+def sum_of_numbers(a,b):
+    return a + b
+print((sum_of_numbers(90,80)))
+
+def square_of_number(x):
+    return x * x    
+print(square_of_number(9))
+
+def multiply_numbers(a, b):
+    return a * b
+print(multiply_numbers(6, 7))
+
+def divide_numbers(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b    
+print(divide_numbers(700,9))
+
+
+
