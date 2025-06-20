c61067bb6a4276d515eef4816baacc8569898bcf
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-20 11:48:39 +0300

    Add files via upload

---
+++ challenge5.py
@@ -0,0 +1,15 @@
+# Challenge 5: Student Information Display 
+ 
+def display_student_info(name, age): 
+    print("Student Name:", name) 
+    print("Student Age:", age) 
+    if age < 18: 
+        print("Status: Minor") 
+    else: 
+        print("Status: Adult") 
+
+ 
+student_name = "Alice" 
+student_age = 20 
+ 
+display_student_info(student_name, student_age)
