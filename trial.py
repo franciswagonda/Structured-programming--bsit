70cb807e8fbb904d3857f88520d13fad944d2bb5
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-06-23 18:00:52 +0300

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,10 @@
+def happy_birthday(name, age):
+    print(f"Happy Birthday, {name}! You are now {age} years old.")
+    print(f'you are now {age} years old! ')
+    print("happy birthday to you!")
+    print()
+    return f"Happy Birthday, {name}!"
+
+happy_birthday("Alice", 30)
+happy_birthday("Bob", 25)
+   
